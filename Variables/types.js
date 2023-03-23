b77f6f11af5a671f9dd23a9b9ef6019fac4a3693
georger <george.ravelly.111@ufrn.edu.br>
2023-03-23 11:30:04 -0300

    Add: tipos primitivos do javascript

---
+++ Variables/types.js
@@ -0,0 +1,13 @@
+/**
+        Tipos de variaveis em javascript
+    Os tipos primitivos do javascript são:
+      1. number - tipo primitivo para representação de numeros;
+      2. string - tipo primitivo para representação de caracteris;
+      3. boolean - tipo primitivo para representar estado logico de verdadeiro ou falso;
+      4. undefined - tipo primitvo que representa uma variavel que não possui qualquer valor atribuido;
+      5. null - tipo primitivo para representar a ausencia de um objeto;
+      6. bigint - O bigint é um tipo mais avançado do javascript, ele é utilizado para representar numeros 
+        gigantes, os quais, o number padrão não da suporte;
+      7. symbol - O tipo symbol é um construtor disponivel no javascript que garante que seu valor será unico, 
+        é utilizado para garantir ao adicionar uma chave a um objeto que ela será unica.
+*/
