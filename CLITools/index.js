0cac78c8c377f03f16b813a2e59d9ede18f84304
georger <george.ravelly.111@ufrn.edu.br>
2023-04-03 10:17:04 -0300

    Add: Cli tools of react

---
+++ CLITools/index.js
@@ -0,0 +1,10 @@
+/**
+    Cli tools javascript são ferramentas que servem para facilitar a criação de projetos que fazem do node,
+  com a escolha de algumas opções o projeto já fica pronto para o desenvolvimento.
+    A forma mais simples de criar um projeto em react é usando a ferramenta npx, com o comando no terminal
+  "npx create 'nome do app'", onde create vem de criar, a ferramenta irá gerar um prejeto em react basico.
+    Outra forma mais avançada de criar um projeto em react é usando Vite, o Vite é uma build tool voltada para
+  aprimorar a experiecia do desenvolvedor atrávez de um desenvolvimento mais rapido e exuto do projeto, além disso
+  ao fazer uso do vite na criaçao do projeto, é possivel personalizar o projeto antes de gera-lo, ao selecionar
+  algumas das opções disponibilizadas pelo Vite.
+*/
