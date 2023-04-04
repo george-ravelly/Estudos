e638cd29e5c8388084eb67cdb289848d2ca524a8
george-ravelly <george.ravelly1@gmail.com>
2023-04-04 12:10:25 -0300

    Add: scopes of javascript

---
+++ Scopes/index.js
@@ -0,0 +1,13 @@
+/**
+ * Escopo no javascript
+ * 
+ *  No javascript existem três formas de escopo:
+ *  
+ *  1. Global - O escopo global representa variaveis e funções que podem ser acesseda de qualquer parte do
+ *  do código;
+ *  2. Function - O escopo function representa as variaveis e funções que estão dentro de uma outra função, 
+ *  estas variaveis e funções só existem dentro da função ¨mãe", portanto, só são acessiveis dentro dela;
+ *  3. Block - O escopo block representa as variaveis que estão definidas dentro de um bloco de codigo, essas
+ *  variaveis não podem ser acesseda de qualquer parte do codigo a não ser dentro do proprio bloco ao qual
+ *  foi criado.
+ */
